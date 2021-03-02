40229326a598ad408f2f5b83e6046146200385bd
Amulek1416 <car16115@byui.edu>
2021-03-01 18:03:28 -0700

    Modified file to build for most OSs

--- DesktopApp/build.py
+++ DesktopApp/build.py
@@ -1,3 +1,24 @@
-import subprocess
+import os
+import sys
 
-print(subprocess.run("pyinstaller --onefile --windowed --icon=\"icons/camera-icon.ico\" --name \"Auto Slide\" main.py"))
+if __name__ == "__main__":
+    NAME = " --name \"Auto Slide\""
+    MAIN_FILE = " main.py"
+    WINDOWS_ICON = " --icon=\"icons/camera-icon.ico\""
+    APPLE_ICON = " --icon=\"icons/camera-icon.icns\""
+
+    GENERAL_COMMAND = "pyinstaller --onefile --windowed"
+
+    WINDOWS_COMMAND = GENERAL_COMMAND + WINDOWS_ICON + NAME + MAIN_FILE
+    APPLE_COMMAND = GENERAL_COMMAND + APPLE_ICON + NAME + MAIN_FILE
+
+    if sys.platform.startswith('win'):
+        print(os.system(WINDOWS_COMMAND))
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        print(os.system(APPLE_COMMAND))
+    elif sys.platform.startswith('darwin'):
+        print(os.system(APPLE_COMMAND))
+    else:
+        raise EnvironmentError('Unsupported platform')
+
